d9b57a4714b7e26b59ddadd6c11b6c24c512fac8
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-06-05 08:08:47 +0200

    inital commit, backend setup

---
+++ src/main/java/ch/bbw/onePass/OnePassApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.onePass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/onePass/OnePassApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.onePass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

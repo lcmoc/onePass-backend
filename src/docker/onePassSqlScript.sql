36c7ea11d887ed42019a275cf7c9182f625b811b
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-06-07 17:35:16 +0200

    example data correction

--- src/docker/onePassSqlScript.sql
+++ src/docker/onePassSqlScript.sql
@@ -102,6 +102,6 @@ INSERT INTO `category` (`name`, `user_id`) VALUES
 INSERT INTO `credentials` (`username`, `password`, `url`, `email`, `notice`, `category_id`, `user_id`) VALUES
 ('user1', 'password1', 'https://example.com', 'user1@example.com', 'Note 1', 1, 1),
 ('user2', 'password2', 'https://example.com', 'user2@example.com', 'Note 2', 1, 1),
-('user3', 'password3', 'https://example.com', 'user3@example.com', 'Note 3', 2, 2),
-('user4', 'password4', 'https://example.com', 'user4@example.com', 'Note 4', 2, 2),
-('user5', 'password5', 'https://example.com', 'user5@example.com', 'Note 5', 3, 3);
+('user3', 'password3', 'https://example.com', 'user3@example.com', 'Note 3', 2, 1),
+('user4', 'password4', 'https://example.com', 'user4@example.com', 'Note 4', 2, 1),
+('user5', 'password5', 'https://example.com', 'user5@example.com', 'Note 5', 3, 2);
--- src/docker/testdata.sql
+++ src/docker/testdata.sql
@@ -1,20 +1,21 @@
--- Insert test data for `user` table
-INSERT INTO `onepass`.`user` (`id`, `email`, `secret_key`) VALUES
-(1, 'user1@example.com', 'secret1'),
-(2, 'user2@example.com', 'secret2'),
-(3, 'user3@example.com', 'secret3');
+-- Testdata for table `user`
+INSERT INTO `user` (`email`, `secret_key`) VALUES
+('user1@example.com', 'secret1'),
+('user2@example.com', 'secret2'),
+('user3@example.com', 'secret3');
 
--- Insert test data for `category` table
-INSERT INTO `onepass`.`category` (`id`, `name`, `user_iduser`) VALUES
-(1, 'Category 1', 1),
-(2, 'Category 2', 1),
-(3, 'Category 3', 2),
-(4, 'Category 4', 3);
+-- Testdata for table `category`
+INSERT INTO `category` (`name`, `user_id`) VALUES
+('Category 1', 1),
+('Category 2', 1),
+('Category 3', 2),
+('Category 4', 2),
+('Category 5', 3);
 
--- Insert test data for `credentials` table
-INSERT INTO `onepass`.`credentials` (`id`, `username`, `password`, `url`, `email`, `notice`, `category_id`) VALUES
-(1, 'user1', 'password1', 'example.com', 'user1@example.com', 'Notice 1', 1),
-(2, 'user2', 'password2', 'example.com', 'user2@example.com', 'Notice 2', 1),
-(3, 'user3', 'password3', 'example.com', 'user3@example.com', 'Notice 3', 2),
-(4, 'user4', 'password4', 'example.com', 'user4@example.com', 'Notice 4', 3),
-(5, 'user5', 'password5', 'example.com', 'user5@example.com', 'Notice 5', 3);
+-- Testdata for table `credentials`
+INSERT INTO `credentials` (`username`, `password`, `url`, `email`, `notice`, `category_id`, `user_id`) VALUES
+('user1', 'password1', 'https://example.com', 'user1@example.com', 'Note 1', 1, 1),
+('user2', 'password2', 'https://example.com', 'user2@example.com', 'Note 2', 1, 1),
+('user3', 'password3', 'https://example.com', 'user3@example.com', 'Note 3', 2, 1),
+('user4', 'password4', 'https://example.com', 'user4@example.com', 'Note 4', 2, 1),
+('user5', 'password5', 'https://example.com', 'user5@example.com', 'Note 5', 3, 2);
